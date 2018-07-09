540fce81f617dac7c3f542b667ead36714a1a0bd
Чайковский Павел Евгеньевич <chapaev@aprovod.local>
2018-07-09 13:57:06 +0300

    folio

--- assets/js/main.js
+++ assets/js/main.js
@@ -277,6 +277,26 @@ $('.onepage-nav').onePageNav({
 });
 
 //*
+// Form send
+//*
+$(document).ready(function() {
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
+});
+
+//*
 // Back to top
 //*
 
